2a8a93a37f3ddbc3569b1114888ee8aab3db7b93
erdalalioglu <erdalalioglu@gmail.com>
2022-03-14 18:11:26 +0100

    Methods

--- src/Day10/MethodsIntro.java
+++ src/Day10/MethodsIntro.java
@@ -5,8 +5,7 @@ public class MethodsIntro {
         topla(3,5);
         topla(9,7);
         topla(5,7);
-        topla(8,9);
-        topla(4,5);
+
 
     }
     public static void topla(int x1,int x2){
