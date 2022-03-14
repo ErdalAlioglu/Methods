68903ac0921c2b26c2843f3a7848df0955395c66
erdalalioglu <erdalalioglu@gmail.com>
2022-03-14 18:06:10 +0100

    Methods

---
+++ src/Day10/MethodsIntro.java
@@ -0,0 +1,17 @@
+package Day10;
+
+public class MethodsIntro {
+    public static void main(String[] args) {
+        topla(3,5);
+        topla(9,7);
+        topla(5,7);
+        topla(8,9);
+
+    }
+    public static void topla(int x1,int x2){
+
+        int toplam1=x1+x2;
+        System.out.println("Toplam:"+toplam1);
+
+    }
+}
